6b48d14f5cd350b04698c58772b6637363370712
mjrt <mjrt.dev@outlook.com>
2025-07-09 00:33:27 +0800

    chore: add permission for gh action, add ut branches coverage > 90%

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+          branches: 90
+        }
+    },
 };
